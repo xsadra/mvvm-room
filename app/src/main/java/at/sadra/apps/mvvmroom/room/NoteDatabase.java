ad693be4fda4612969b58e874f2da31f35ee000b
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-17 00:11:10 +0200

    Add NoteDatabase

---
+++ app/src/main/java/at/sadra/apps/mvvmroom/room/NoteDatabase.java
@@ -0,0 +1,27 @@
+package at.sadra.apps.mvvmroom.room;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import at.sadra.apps.mvvmroom.model.Note;
+
+@Database(entities = {Note.class}, version = 1)
+public abstract class NoteDatabase extends RoomDatabase {
+
+    private static NoteDatabase instance;
+
+    public abstract NoteDao noteDao();
+
+    public static synchronized NoteDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(),
+                    NoteDatabase.class,
+                    "note_database")
+                    .fallbackToDestructiveMigration()
+                    .build();
+        }
+        return instance;
+    }
+}
