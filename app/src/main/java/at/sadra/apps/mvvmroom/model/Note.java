29596c53a8803dc9ca5f7ec72430c3e40951947e
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-16 21:14:03 +0200

    Add Note Entity to model(s)

---
+++ app/src/main/java/at/sadra/apps/mvvmroom/model/Note.java
@@ -0,0 +1,61 @@
+package at.sadra.apps.mvvmroom.model;
+
+import java.util.Objects;
+
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "note_table")
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String title;
+
+    private String description;
+
+    //@ColumnInfo(name = "priority_column")
+    private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return priority == note.priority &&
+                Objects.equals(title, note.title) &&
+                Objects.equals(description, note.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, description, priority);
+    }
+}
