9dab3311cd93b664f230a6fd49c864da244ff9b4
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-16 20:23:11 +0200

    Create New Android Project

---
+++ app/src/main/java/at/sadra/apps/mvvmroom/MainActivity.java
@@ -0,0 +1,14 @@
+package at.sadra.apps.mvvmroom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
