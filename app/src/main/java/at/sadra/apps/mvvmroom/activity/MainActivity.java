3cea2b97d4e1f19c0db5a27f995d28a99fe553b8
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-17 17:24:41 +0200

    Refactor Main Activity to Load Data into RecyclerView

--- app/src/main/java/at/sadra/apps/mvvmroom/activity/MainActivity.java
+++ app/src/main/java/at/sadra/apps/mvvmroom/activity/MainActivity.java
@@ -7,30 +7,38 @@ import java.util.List;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import at.sadra.apps.mvvmroom.R;
+import at.sadra.apps.mvvmroom.adapter.NoteAdapter;
 import at.sadra.apps.mvvmroom.app.App;
 import at.sadra.apps.mvvmroom.model.Note;
 import at.sadra.apps.mvvmroom.viewmodel.NoteViewModel;
 
 public class MainActivity extends AppCompatActivity {
 
-    private NoteViewModel noteViewModel;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState );
         setContentView(R.layout.activity_main);
         initialize();
+
     }
 
     private void initialize() {
 
-        noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        RecyclerView noteRecyclerView = findViewById(R.id.note_recycler_view);
+        noteRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        noteRecyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        noteRecyclerView.setAdapter(adapter);
+
+        NoteViewModel noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
         noteViewModel.getAllNote().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
-                // TODO: update RecyclerView
-                App.toast(MainActivity.this, App.Message.ON_CHANGED);
+                adapter.setNotes(notes);
             }
         });
     }
