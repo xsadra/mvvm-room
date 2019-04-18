262d0211cb28e09320346cd5215629cd74ccb994
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-18 14:52:52 +0200

    Add new Activity for Add Note

---
+++ app/src/main/java/at/sadra/apps/mvvmroom/activity/AddNoteActivity.java
@@ -0,0 +1,15 @@
+package at.sadra.apps.mvvmroom.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import at.sadra.apps.mvvmroom.R;
+
+import android.os.Bundle;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
