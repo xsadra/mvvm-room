c33b7d09b3d880d2fe51fd57bf71ca65d1c3ff44
Sadra <31988724+xsadra@users.noreply.github.com>
2019-04-21 22:40:13 +0200

    Add Some Tags and Messages to App Class.

--- app/src/main/java/at/sadra/apps/mvvmroom/app/App.java
+++ app/src/main/java/at/sadra/apps/mvvmroom/app/App.java
@@ -18,10 +18,23 @@ public class App {
 
     public static class Strings {
         public static final String TAG = "NOTE";
+        public static final String ADD_NOTE_TITLE = "Add Note";
+        public static final String APP_TITLE = "Note";
+    }
+
+    public static class Tag {
+        public static final int ADD_NOTE_REQUEST = 1;
+
+        public static final String EXTRA_TITLE = "at.sadra.apps.mvvmroom.activity.EXTRA_TITLE";
+        public static final String EXTRA_DESCRIPTION = "at.sadra.apps.mvvmroom.activity.EXTRA_DESCRIPTION";
+        public static final String EXTRA_PRIORITY = "at.sadra.apps.mvvmroom.activity.EXTRA_PRIORITY";
+
     }
 
     public static class Message {
         public static final String ON_CHANGED = "onChanged";
+        public static final String NOTE_SAVED = "Note saved!";
+        public static final String NOTE_NOT_SAVED = "Note not Saved!";
     }
 
     public static class Error {
